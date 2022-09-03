19c2cfedc14d9c741b2a098a319c9ef9994936ee
borbinhaa <gabrieldeborba07@gmail.com>
2022-09-02 23:19:22 -0300

    Adicionar telas de update e cadastro de docentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,37 @@
-import { LayoutFullwidthModule } from './layouts/layout-fullwidth/layout-fullwidth.module';
 import { LayoutDefaultModule } from './layouts/layout-default/layout-default.module';
+import { LayoutFullwidthModule } from './layouts/layout-fullwidth/layout-fullwidth.module';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { InputTextModule } from 'primeng/inputtext';
-import { CalendarModule } from 'primeng/calendar';
+import { AccordionModule } from 'primeng/accordion';
 import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
 import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
+import { MessagesModule } from 'primeng/messages';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
-import { AccordionModule } from 'primeng/accordion';
-import { MessagesModule } from 'primeng/messages';
-import { MessageModule } from 'primeng/message';
+
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputMaskModule } from 'primeng/inputmask';
+import { TeacherFormComponent } from './components/teacher/teacher-form/teacher-form.component';
+import { TeacherCreateComponent } from './views/teacher/teacher-create/teacher-create.component';
+import { TeacherUpdateComponent } from './views/teacher/teacher-update/teacher-update.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TeacherCreateComponent,
+    TeacherFormComponent,
+    TeacherUpdateComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +45,10 @@ import { MessageModule } from 'primeng/message';
     InputTextModule,
     CalendarModule,
     ButtonModule,
-    DropdownModule
+    DropdownModule,
+    ReactiveFormsModule,
+    CheckboxModule,
+    InputMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
