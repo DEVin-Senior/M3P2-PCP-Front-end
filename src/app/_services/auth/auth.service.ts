cbde9e54145055458ada05515f000ca35e789e48
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2022-09-10 14:08:04 -0300

    ALL-FRONTEND-16 - Remover consoles.log do serviço de login e auth

--- src/app/_services/auth/auth.service.ts
+++ src/app/_services/auth/auth.service.ts
@@ -5,12 +5,11 @@ import { User } from 'src/app/components/user/user.model';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  apiURL : string = environment.apiBaseURL + "loginRegister";
-  tokenFullUrl : string = environment.apiBaseURL + environment.tokenUrl;
+  apiURL: string = environment.apiBaseURL + 'loginRegister';
+  tokenFullUrl: string = environment.apiBaseURL + environment.tokenUrl;
   clientId: string = environment.clientId;
   clientSecret: string = environment.clientSecret;
 
@@ -18,37 +17,33 @@ export class AuthService {
 
   user: User = new User();
 
-  constructor(
-    private http: HttpClient, 
-  ) { }
+  constructor(private http: HttpClient) {}
 
   isAuthenticated(): boolean {
     return this.isLoggedIn;
   }
 
-  logOut(){
+  logOut() {
     localStorage.removeItem('access_token');
   }
 
-  saveUser(user: User) : Observable<any>{
-    console.log(`Name: ${user.name} User AUTHSERVICE: ${user.email}, Password: ${user.password}`);
-      let bodyString = JSON.stringify(user);
-      let headers = new HttpHeaders({ 'Content-Type': 'application/JSON' });
-      return this.http.post<any>(this.apiURL, bodyString, { headers: headers });
+  saveUser(user: User): Observable<any> {
+    let bodyString = JSON.stringify(user);
+    let headers = new HttpHeaders({ 'Content-Type': 'application/JSON' });
+    return this.http.post<any>(this.apiURL, bodyString, { headers: headers });
   }
 
-  login(username: string, password: string) : Observable<any>{
+  login(username: string, password: string): Observable<any> {
     const params = new HttpParams()
-                  .set('username', username)
-                  .set('password', password)
-                  .set('grant_type', 'password');
+      .set('username', username)
+      .set('password', password)
+      .set('grant_type', 'password');
 
     const headers = {
-      'Authorization' : 'Basic ' + btoa(`${this.clientId}:${this.clientSecret}`),
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
+      Authorization: 'Basic ' + btoa(`${this.clientId}:${this.clientSecret}`),
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
 
-    return this.http.post(this.tokenFullUrl, params, { headers })
+    return this.http.post(this.tokenFullUrl, params, { headers });
   }
-  
 }
