c15e044a5acf67600f5a130e10ac21503070ee71
RicardoWashington <95726921+RicardoWashington@users.noreply.github.com>
2022-09-11 16:30:39 -0300

    SQ02-07 - Testes unitários com Jasmine e Karma para o componente teacher-list

--- src/app/components/teacher/teacher-list/teacher-list.component.spec.ts
+++ src/app/components/teacher/teacher-list/teacher-list.component.spec.ts
@@ -1,23 +1,49 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
-// import { TeacherListComponent } from './teacher-list.component';
+import { TeacherListComponent } from './teacher-list.component';
 
-// describe('TeacherListComponent', () => {
-//   let component: TeacherListComponent;
-//   let fixture: ComponentFixture<TeacherListComponent>;
+describe('TeacherListComponent', () => {
+  let component: TeacherListComponent;
+  let fixture: ComponentFixture<TeacherListComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ TeacherListComponent ]
-//     })
-//     .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+        imports: [
+            RouterTestingModule,
+            HttpClientTestingModule
+          ],
+      declarations: [ TeacherListComponent ]
+    })
+    .compileComponents();
 
-//     fixture = TestBed.createComponent(TeacherListComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(TeacherListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`#${TeacherListComponent.prototype.setArchived.name} validate status of archived is true then change to false`, () => {
+    component.setArchived('1', true);
+
+    expect(component.patchTeacher.archived).toEqual(false);
+  });
+
+  it(`#${TeacherListComponent.prototype.setArchived.name} validate status of archived is false then change to true`, () => {
+    component.setArchived('1', false);
+
+    expect(component.patchTeacher.archived).toEqual(true);
+  });
+
+  it(`#${TeacherListComponent.prototype.ngOnInit.name} Should present the button 'Novo Docente' `, () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button').textContent).toContain(
+      'Novo Docente'
+    );
+  });
+});
