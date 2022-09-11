72c75b5c3229e469028fe194d22b2023122e21b3
Eduardo <eduardoamorim927@gmail.com>
2022-09-11 12:38:13 -0300

    #SQ03-55 - Implementação de mensagem de erro nas telas de create e update

--- src/app/components/courseclass/courseclass-update/courseclass-update.component.ts
+++ src/app/components/courseclass/courseclass-update/courseclass-update.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SelectItem } from 'primeng/api';
 import { Observable } from 'rxjs';
+import { IAlert } from 'src/app/_interfaces/alert/iAlert';
+import { AlertService } from 'src/app/_shared/alert/alert.service';
+import { WARNING } from 'src/environments/environment';
 import { CourseClassService } from '../courseclass.service';
 import { CourseclassUpdateContextService } from '../courseclassUpdate-context.service';
 import {CourseClassUpdateDto} from '../dto/courseclass-update.model'
@@ -14,6 +17,7 @@ import {CourseClassUpdateDto} from '../dto/courseclass-update.model'
 })
 export class CourseClassUpdateComponent implements OnInit {
   
+  alertMessage!: IAlert;
   regExp: RegExp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
   globalInitialdate:any;
   globalEndDate:any;
@@ -39,7 +43,8 @@ export class CourseClassUpdateComponent implements OnInit {
     private courseclassUpdateContextService : CourseclassUpdateContextService,
     private classCourseService: CourseClassService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private alertService: AlertService
   ) { }
 
   ngOnInit(): void {  //ngOnInit está comentado aguardando configuração de backend
@@ -80,7 +85,12 @@ export class CourseClassUpdateComponent implements OnInit {
     this.courseclassUpdateContextService.setCourseClass(this.courseClass);    
     this.router.navigate(['/layout/turmas/atualizar/' + this.route.snapshot.paramMap.get('id') + '/modulo']);
    }else{
-    this.classCourseService.errorHandler;
+    this.alertMessage = {
+      title: 'Atenção',
+      message: 'Preencha todos os campos obrigatórios.',
+      typeAlert: WARNING,
+    };
+    this.alertService.showGenericAlert(this.alertMessage);
    }
 
    
