5fdb9bde9350a37fb27e5a717004c862eb75c820
Matheus Freitas <49175829+MatheusFilipeFreitas@users.noreply.github.com>
2022-09-11 02:11:45 -0300

    Update courseclass-update.component.ts

--- src/app/components/courseclass/courseclass-update/courseclass-update.component.ts
+++ src/app/components/courseclass/courseclass-update/courseclass-update.component.ts
@@ -4,10 +4,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { SelectItem } from 'primeng/api';
 import { Observable } from 'rxjs';
 import { CourseClassService } from '../courseclass.service';
-<<<<<<< HEAD
-import { CourseClassReadDto } from '../dto/courseclass-read.model';
-=======
->>>>>>> d91e30d8ceea32fa50033d4c736bdf30bc3c06b8
 import { CourseclassUpdateContextService } from '../courseclassUpdate-context.service';
 import {CourseClassUpdateDto} from '../dto/courseclass-update.model'
 
@@ -18,10 +14,7 @@ import {CourseClassUpdateDto} from '../dto/courseclass-update.model'
 })
 export class CourseClassUpdateComponent implements OnInit {
   
-<<<<<<< HEAD
   regExp: RegExp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-=======
->>>>>>> d91e30d8ceea32fa50033d4c736bdf30bc3c06b8
   globalInitialdate:any;
   globalEndDate:any;
 
